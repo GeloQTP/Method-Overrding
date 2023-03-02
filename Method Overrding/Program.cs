89f08bdcf51ce9a02c0fd2a4e682786986ba3e5f
Gelo <geloadrian24@gmail.com>
2023-03-02 21:24:51 +0800

    Add project files.

---
+++ Method Overrding/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace Method_Overriding // 3-2-23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();    
+            Cat cat = new Cat();
+            Pig pig = new Pig();   
+
+            dog.speak();
+            cat.speak();
+            pig.speak();
+        }
+    }
+}
+
+ abstract class Animal // you can also use 'virtual' so you don't have to put anything on the class name
+{
+    public abstract void speak(); // public virtual void speak(){ 'algorithm is required' }
+    
+}
+
+/*
+ *  class Animal // another wait overriding
+  {
+    public virtual void speak()
+    {
+        // algorithm
+    }   
+
+   }
+ */
+
+class Dog : Animal 
+{ 
+    public override void speak()
+    {
+        Console.WriteLine("THe Dog goes *bark*");
+    }
+}
+
+class Cat : Animal
+{
+    public override void speak()
+    {
+        Console.WriteLine("The Cat goes *meow*");
+    }  
+}
+
+class Pig : Animal
+{
+    public override void speak()
+    {
+        Console.WriteLine("The Pig goes *oink*");
+    }
+}
